f130450706bbed2d5f2a8cd4542674a6fd76fea3
Danila <danila.fafr@gmail.com>
2025-12-17 11:01:23 +0300

    Initial Commit

---
+++ Travel/TravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelApp.swift
+//  Travel
+//
+//  Created by Данила Спиридонов on 17.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct TravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
